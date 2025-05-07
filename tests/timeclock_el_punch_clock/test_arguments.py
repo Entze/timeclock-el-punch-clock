1c2230d6b668e5442c05477a97160f979f16a66d
Lukas Grassauer <lukas@grassauer.eu>
2025-05-07 15:36:56 +0200

    Add missing exception test cases for from_namespace

--- tests/timeclock_el_punch_clock/test_arguments.py
+++ tests/timeclock_el_punch_clock/test_arguments.py
@@ -1,8 +1,11 @@
 import argparse
 import datetime
 import pathlib
+import stat
 import tempfile
 from unittest import mock
+
+import pytest
 
 from timeclock_el_punch_clock.paths import writeable_file_paths
 from timeclock_el_punch_clock.arguments import Arguments
@@ -52,6 +55,57 @@ def test_from_namespace_with_file_returns_command_with_file() -> None:
             actual = Arguments.from_namespace(namespace)
 
     assert actual == expected
+
+
+def test_from_namespace_with_non_existing_file_raises_exception_group_with_path_does_not_exist() -> (
+    None
+):
+    path = pathlib.Path("file-does-not-exist.txt")
+    namespace = argparse.Namespace(file=path)
+
+    with pytest.raises(ExceptionGroup) as excinfo:
+        Arguments.from_namespace(namespace)
+        assert str(path) in excinfo.value, (
+            f"ExceptionGroup shows the path: {excinfo.value}"
+        )
+        assert "does not exist" in excinfo.value, (
+            f"ExceptionGroup shows the reason: {excinfo.value}"
+        )
+
+
+def test_from_namespace_with_directory_raises_exception_group_with_not_a_file_error() -> (
+    None
+):
+    with tempfile.TemporaryDirectory() as directory:
+        path = pathlib.Path(directory)
+        namespace = argparse.Namespace(file=path)
+
+        with pytest.raises(ExceptionGroup) as excinfo:
+            Arguments.from_namespace(namespace)
+            assert str(path) in excinfo.value, (
+                f"ExceptionGroup.value shows the path: {excinfo.value}"
+            )
+            assert "is not a file" in excinfo.value, (
+                f"ExceptionGroup shows the reason: {excinfo.value}"
+            )
+
+
+def test_from_namespace_with_non_readable_file_raises_exception_group_with_not_writeable_error() -> (
+    None
+):
+    with tempfile.NamedTemporaryFile(mode="r") as file:
+        path = pathlib.Path(file.name)
+        path.chmod(stat.S_IREAD)
+        namespace = argparse.Namespace(file=path)
+
+        with pytest.raises(ExceptionGroup) as excinfo:
+            Arguments.from_namespace(namespace)
+            assert str(path) in excinfo.value, (
+                f"ExceptionGroup.value shows the path: {excinfo.value}"
+            )
+            assert "is not writeable" in excinfo.value, (
+                f"ExceptionGroup shows the reason: {excinfo.value}"
+            )
 
 
 def test_from_namespace_with_datetimestamp_returns_command_with_datetimestamp() -> None:
