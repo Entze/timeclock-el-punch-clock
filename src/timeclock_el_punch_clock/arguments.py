88e60cc50ddf3953d5a34d22b8694a748e5b06b0
Lukas Grassauer <lukas@grassauer.eu>
2025-05-07 14:30:01 +0200

    Add from_namespace in the case of an empty namespace

--- src/timeclock_el_punch_clock/arguments.py
+++ src/timeclock_el_punch_clock/arguments.py
@@ -1,8 +1,54 @@
-from dataclasses import dataclass
-
+import argparse
+import datetime
 import pathlib
+from collections.abc import Sequence
+from dataclasses import dataclass
+from typing import Self, MutableSequence, Callable
 
+import deal
+
+import timeclock_el_punch_clock.paths.writeable_file_paths as writeable_file_paths
+from timeclock_el_punch_clock.accounts import Account
+from timeclock_el_punch_clock.paths.errors import NotAFileError, NotWriteableError
+from timeclock_el_punch_clock.paths.writeable_file_paths import WriteableFilePath
 
 @dataclass(frozen=True)
 class Arguments:
-    file: pathlib.Path
+    file: WriteableFilePath
+    datetimestamp: datetime.datetime
+    accounts: Sequence[Account]
+    delimiter: str
+
+    @staticmethod
+    def default_file() -> pathlib.Path:
+        return pathlib.Path("log.timeclock")
+
+    @staticmethod
+    def default_datetimestamp() -> datetime.datetime:
+        return datetime.datetime.now()
+
+    @staticmethod
+    def default_accounts() -> Sequence[Account]:
+        return ()
+
+    @staticmethod
+    def default_delimiter() -> str:
+        return ":"
+
+    @classmethod
+    @deal.has("io")
+    @deal.raises(ExceptionGroup)
+    def from_namespace(cls, namespace: argparse.Namespace) -> Self:
+        # errors: MutableSequence[_ArgumentsError] = []
+
+        file: WriteableFilePath
+        datetimestamp: datetime.datetime
+        accounts: Sequence[Account]
+        delimiter: str
+
+        file = writeable_file_paths.from_path(cls.default_file())
+        datetimestamp = cls.default_datetimestamp()
+        accounts = cls.default_accounts()
+        delimiter = cls.default_delimiter()
+
+        return cls(file=file, datetimestamp=datetimestamp, accounts=accounts, delimiter=delimiter)
---
+++ tests/timeclock_el_punch_clock/test_arguments.py
@@ -0,0 +1,26 @@
+import argparse
+import datetime
+import pathlib
+import tempfile
+from unittest import mock
+
+from timeclock_el_punch_clock.paths import writeable_file_paths
+from timeclock_el_punch_clock.arguments import Arguments
+
+_default_datetimestamp = datetime.datetime(1970, 1, 1, 0, 0, 0)
+
+def test_from_namespace_with_empty_namespace_returns_default() -> None:
+    namespace = argparse.Namespace()
+    with (tempfile.NamedTemporaryFile(mode='w') as file,
+          mock.patch.object(Arguments, "default_file", return_value=pathlib.Path(file.name)),
+          mock.patch.object(Arguments, "default_datetimestamp", return_value=_default_datetimestamp)):
+        expected = Arguments(
+            file=writeable_file_paths.from_path(Arguments.default_file()),
+            datetimestamp=Arguments.default_datetimestamp(),
+            accounts=Arguments.default_accounts(),
+            delimiter=Arguments.default_delimiter(),
+        )
+
+        actual = Arguments.from_namespace(namespace)
+
+    assert actual == expected
